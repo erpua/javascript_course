bee0bcc603a377ad0c8ab8842f2f84b976a71ef2
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-02-01 17:55:09 -0600

    10/272 types

--- 1. console.js
+++
@@ -1,7 +0,0 @@
-3 + 2;
-
-console.log(3+2);
-
-console.log('Ievgen');
-
-console.log('Hello World');
---
+++ index.js
@@ -0,0 +1,22 @@
+/* 3 + 2;
+
+<hr/>
+
+console.log(3+2);
+
+console.log('Ievgen');
+
+console.log('Hello World'); */
+
+//variables
+
+let a;
+
+a = true;
+
+console.log(a);
+
+{
+    a=10,
+    b=5
+}
