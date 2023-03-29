973f9a3651ecde367aec6a62260b3437219b47cd
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-03-29 07:09:56 -0600

    added practice

---
+++ 7_practice.js
@@ -0,0 +1,33 @@
+const objA = {
+    a: 10,
+    b: true
+};
+
+const copyOfA = objA;
+
+copyOfA.a = 20;
+// 20
+
+copyOfA.c = 20;
+
+
+console.log(objA);
+// { a: 20, b: true, c: 20 }
+
+let g = 10;
+
+g = true;
+
+g = 'Bogdan';
+
+g = undefined;
+
+function greetWorld() {
+    console.log("Hello World!");
+};
+
+a(); // Hello World!
+
+a = 10;
+
+a(); // Error: a is not a function
