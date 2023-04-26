a67bf49c9960e357e806297652f7517c26feb982
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-04-26 12:58:11 -0600

    added changes

---
+++ 11_object_methods.js
@@ -0,0 +1,8 @@
+const myCity = {
+	city: 'New york',
+	cityGreeting: function () {
+		console.log('The history of New York begins around 10,000 B.C. when the first people arrived.');
+	}
+};
+
+myCity.cityGreeting();
