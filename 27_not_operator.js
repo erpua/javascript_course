ace1ec5faef3054b1e89a2ed6d8ebb72687d65d9
Ievgen Tovtin <zhe.tovtin@gmail.com>
2023-05-29 18:30:59 -0600

    added not and short-cicuit

---
+++ 27_not_operator.js
@@ -0,0 +1,17 @@
+!10 // false
+!0 // true
+!'abc' //false
+!'' // true
+!true // false
+!undefined // true
+
+// ---
+
+!!10 //true
+!!0 // false
+!!'abc' // true
+!!'' // false
+!!true // true
+!!undefined //false
+
+console.log('!!{} => ', !!{}); //true
---
+++ 28_short-cricuit_operator.js
@@ -0,0 +1,1 @@
+expression1 && expression2 // if 1 of 2 is not true => all is false
